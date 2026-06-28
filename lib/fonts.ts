102c5f464a5485fc19436ab785e211580145278d
GamerX <mrvimal0501@gmail.com>
2026-06-28 08:17:05 +0530

    feat: create application foundation

---
+++ lib/fonts.ts
@@ -0,0 +1,12 @@
+import { Cormorant_Garamond, Inter } from "next/font/google";
+
+export const headingFont = Cormorant_Garamond({
+  subsets: ["latin"],
+  variable: "--font-heading",
+  weight: ["400", "500", "600", "700"],
+});
+
+export const bodyFont = Inter({
+  subsets: ["latin"],
+  variable: "--font-body",
+});
